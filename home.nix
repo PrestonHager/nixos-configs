a35f1041447cb126b44b19c2de54617c863ee4b1
Preston Hager <preston@hagerfamily.com>
2024-07-05 15:48:07 -0600

    Added GitHub CLI to home profile

--- home.nix
+++ home.nix
@@ -36,6 +36,8 @@
 
     tree
 
+    gh
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
