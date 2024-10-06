05fbefcff15e9272663783c8daf7e9eee6ad75fe
Preston Hager <preston@hagerfamily.com>
2024-10-06 04:03:16 -0600

    Added auto-lock on key removal
    Only locks the desktop if a key is removed, not required to login with a
    key, you can still login via password.

--- nixos/default.nix
+++ nixos/default.nix
@@ -22,16 +22,30 @@
   ];
 
   # Enable login via hardware keys
-  security.pam.services = {
-    login.u2fAuth = true;
-    sudo.u2fAuth = true;
+  security.pam = {
+    services = {
+      login.u2fAuth = true;
+      sudo.u2fAuth = true;
+    };
+    u2f.enable = true;
+    # Enable a prompt for a hardware key
+    u2f.cue = true;
   };
-  # Enable a prompt for a hardware key
-  security.pam.u2f.cue = true;
   # Enable pcsclite for smart card support
   services.pcscd = {
     enable = true;
   };
+
+  # Only allow user to login with a key by locking the desktop whenever a key is
+  # unplugged. Note that you can still get into the system via normal
+  # credentials. Only if a key is already connected and then disconnected with
+  # the desktop lock the current session.
+  services.udev.extraRules = ''
+    ACTION=="remove",\
+      SUBSYSTEM=="usb",\
+      ENV{PRODUCT}=="1050/407/543",\
+      RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+  '';
 
   # Configure zsh for the users by default
   users.defaultUserShell = pkgs.zsh;
