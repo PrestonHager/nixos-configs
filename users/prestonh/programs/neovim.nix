2b496814dd61c582e78a56f8e263150e6351369e
Preston Hager <preston@hagerfamily.com>
2024-10-22 00:22:11 -0600

    Updated tmux/neovim configs to use Alt+nav keybind
    Changed vim-tmux-navigator keybindings from Ctrl+h/j/k/l to Alt+h/j/k/l
    in both the tmux and vim sides. Now you can still clear the terminal
    with Ctrl+l!

--- users/prestonh/programs/neovim.nix
+++ users/prestonh/programs/neovim.nix
@@ -1,29 +1,45 @@
 { config, pkgs, lib ? pkgs.lib, ... }:
 
 {
-    programs.neovim = {
-        enable = true;
-	withNodeJs = true;
-	viAlias = true;
-	vimAlias = true;
-	plugins = with pkgs.vimPlugins; [
-            nvim-cmp
-	    obsidian-nvim
-	    telescope-nvim
-	    vim-tmux-navigator
-	    nvim-treesitter
-	];
-	extraConfig = ''
-	    set number
-	    set tabstop=2 softtabstop=2 shiftwidth=2
-	    set expandtab
-	    set textwidth=80
-	    set clipboard=xclip
-	'';
-    };
+  programs.neovim = {
+    enable = true;
+    withNodeJs = true;
+    viAlias = true;
+    vimAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      nvim-cmp
+      obsidian-nvim
+      telescope-nvim
+      vim-tmux-navigator
+      copilot-vim
+      nvim-treesitter.withAllGrammars
+    ];
+    extraConfig = ''
+      set number
+      set tabstop=2 softtabstop=2 shiftwidth=2
+      set expandtab
+      set textwidth=80
+      set clipboard^=unnamed,unnamedplus
 
-    # Allow unfree packages for certain plugins
-#    nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-#    ];
+      let g:tmux_navigator_no_mappings = 1
+
+      nnoremap <silent> <M-h> :TmuxNavigateLeft<cr>
+      nnoremap <silent> <M-j> :TmuxNavigateDown<cr>
+      nnoremap <silent> <M-k> :TmuxNavigateUp<cr>
+      nnoremap <silent> <M-l> :TmuxNavigateRight<cr>
+      nnoremap <silent> <M-\> :TmuxNavigatePrevious<cr>
+    '';
+    extraLuaConfig = ''
+      -- Enable treesitter highlighting
+      require('nvim-treesitter.configs').setup({
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    '';
+  };
 }
 
