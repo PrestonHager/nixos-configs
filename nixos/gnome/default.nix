270bcd081f7c9055cf667d41eed5774e6e8268cc
Preston Hager <preston@hagerfamily.com>
2025-02-05 17:55:31 -0700

    Added spotify and hardware control center programs
    Added mcontrolcenter and spotify to the user programs.
    Enabled associated system wide options for program installation.

--- nixos/gnome/default.nix
+++ nixos/gnome/default.nix
@@ -5,4 +5,20 @@
     ./desktop.nix
     ./programs/steam.nix
   ];
+
+  environment.systemPackages = with pkgs; [
+    # MSI-mission control center
+    mcontrolcenter
+  ];
+
+  # Uncomment if needed
+  #services.openssh = {
+  #  enable = true;
+  #  settings = {
+  #    PasswordAuthentication = true;
+  #    KbdInteractiveAuthentication = false;
+  #    # Only enable if absolutely needed
+  #    # PermitRootLogin = true;
+  #  };
+  #};
 }
