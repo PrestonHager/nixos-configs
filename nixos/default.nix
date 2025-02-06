e3761ac0ffb6a02913c8ec258588e5456ed40af6
Preston Hager <preston@hagerfamily.com>
2025-02-05 17:56:50 -0700

    Enabled systemd OOM killer

--- nixos/default.nix
+++ nixos/default.nix
@@ -56,6 +56,9 @@
   # Enable network manager
   networking.networkmanager.enable = true;
 
+  # Enable the OOM killer
+  systemd.oomd.enable = true;
+
   # Enable experimental features
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
