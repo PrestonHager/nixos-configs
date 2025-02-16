438294da437449896d7678407b0ec74886625d10
Preston Hager <preston@hagerfamily.com>
2025-02-16 01:30:46 -0700

    Updated neovim configuration
    Created a let variable for plugins and precompiled tree-sitter parsers.
    Added move language to the tree-sitter parsers.
    Reorganized home manager files for better access.

--- users/prestonh/home.nix
+++ users/prestonh/home.nix
@@ -3,9 +3,9 @@
 {
   # Import other home manager files
   imports = [
-    ./home-config.nix
-    ./home-programs.nix
     ./user-packages.nix
+    ./home-programs.nix
+    ./home-config.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
@@ -22,38 +22,7 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    # Command line tools
-    # zoxide is the new cd command allowing fuzzy directory changes
-    zoxide
-    # fzf (fuzzy finder) allows finding of files via a search box
-    fzf
-    # lsd (ls-deluxe) makes listings colorful and easier to read
-    lsd
-    # list the directory structure of directory in a branching/tree format
-    tree
-    # thefuck is a fun command corrector, trying to guess what you meant when
-    # mistyping a command
-    thefuck
-    # markdown viewer (with edit button)
-    glow
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
+
--- users/prestonh/user-packages.nix
+++ users/prestonh/user-packages.nix
@@ -6,6 +6,25 @@
   ];
 
   home.packages = with pkgs; [
+    # Command line tools
+    # zoxide is the new cd command allowing fuzzy directory changes
+    zoxide
+    # fzf (fuzzy finder) allows finding of files via a search box
+    fzf
+    # Startship is a prompt tool
+    starship
+    # Carapace, cross-shell autocomplete system
+    carapace
+    # lsd (ls-deluxe) makes listings colorful and easier to read
+    lsd
+    # list the directory structure of directory in a branching/tree format
+    tree
+    # thefuck is a fun command corrector, trying to guess what you meant when
+    # mistyping a command
+    thefuck
+    # markdown viewer (with edit button)
+    glow
+
     # GitHub CLI
     gh
     gnupg
@@ -27,8 +46,11 @@
 
     # MPV crossplatform multimedia player
     mpv
+
     # htop program
     htop-vim
+    # rip-grep
+    ripgrep
 
     # Packages for the GPU gnome extension
     lm_sensors
