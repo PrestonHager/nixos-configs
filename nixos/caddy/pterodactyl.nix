4c6a5e8aa3ed3b09c174bb5ef733e95b76bef47c
Preston Hager <preston@hagerfamily.com>
2026-06-01 15:01:27 -0600

    Add test.panel.prestonhager.com for panel fork in Projects

--- nixos/caddy/pterodactyl.nix
+++ nixos/caddy/pterodactyl.nix
@@ -11,48 +11,57 @@ let
       port = "443";
     };
   };
+
+  panelSite = { hostPath, containerPath }:
+    ''
+      root * ${hostPath}
+
+      file_server
+
+      php_fastcgi localhost:9001 {
+          root ${hostPath}
+          index index.php
+
+          env SCRIPT_FILENAME ${containerPath}/public{path}
+          env DOCUMENT_ROOT ${containerPath}/public
+
+          env PHP_VALUE "upload_max_filesize = 100M
+          post_max_size = 100M"
+          env HTTP_PROXY ""
+          env HTTPS "on"
+
+          read_timeout 300s
+          dial_timeout 300s
+          write_timeout 300s
+      }
+
+      header Strict-Transport-Security "max-age=16768000; preload;"
+      header X-Content-Type-Options "nosniff"
+      header X-XSS-Protection "1; mode=block;"
+      header X-Robots-Tag "none"
+      header Content-Security-Policy "frame-ancestors 'self'"
+      header X-Frame-Options "DENY"
+      header Referrer-Policy "same-origin"
+
+      request_body {
+          max_size 100m
+      }
+
+      respond /.ht* 403
+    '';
 in {
-  # Add caddy to the pterodactyl user group so it can access the files
   users.users.caddy.extraGroups = [ "pterodactyl" ];
 
   services.caddy = {
     virtualHosts = {
-      "panel.prestonhager.com".extraConfig = ''
-        root * /pterodactyl/html/public
-
-        file_server
-
-        php_fastcgi localhost:9001 {
-            root /pterodactyl/html/public
-            index index.php
-
-            env SCRIPT_FILENAME /var/www/pterodactyl/public{path}
-            env DOCUMENT_ROOT /var/www/pterodactyl/public
-
-            env PHP_VALUE "upload_max_filesize = 100M
-            post_max_size = 100M"
-            env HTTP_PROXY ""
-            env HTTPS "on"
-
-            read_timeout 300s
-            dial_timeout 300s
-            write_timeout 300s
-        }
-
-        header Strict-Transport-Security "max-age=16768000; preload;"
-        header X-Content-Type-Options "nosniff"
-        header X-XSS-Protection "1; mode=block;"
-        header X-Robots-Tag "none"
-        header Content-Security-Policy "frame-ancestors 'self'"
-        header X-Frame-Options "DENY"
-        header Referrer-Policy "same-origin"
-
-        request_body {
-            max_size 100m
-        }
-
-        respond /.ht* 403
-      '';
+      "panel.prestonhager.com".extraConfig = panelSite {
+        hostPath = "/pterodactyl/html/public";
+        containerPath = "/var/www/pterodactyl";
+      };
+      "test.panel.prestonhager.com".extraConfig = panelSite {
+        hostPath = "/home/prestonh/Projects/panel/public";
+        containerPath = "/var/www/pterodactyl-test";
+      };
     } // builtins.mapAttrs (websocketHost: websocketConfig: {
       extraConfig = ''
         @ws path_regexp wsSuffix .*/ws(/.*)?$
@@ -76,4 +85,3 @@ in {
     }) websocketHosts;
   };
 }
-
