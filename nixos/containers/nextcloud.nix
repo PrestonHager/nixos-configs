01dd18a1feda72fb994c925a039fac0af9c3715e
Preston Hager <preston@hagerfamily.com>
2026-06-30 22:13:15 -0600

    ace: bump nextcloud to 34.0.1
    Automated patch/minor update triggered by ace-service-auto-update.

--- nixos/containers/nextcloud.nix
+++ nixos/containers/nextcloud.nix
@@ -21,7 +21,7 @@ let
     chmod +x $out/bin/rclone
   '';
   # 34.0.1 not published on Docker Hub (see nextcloud/docker#2584); use latest 34.0.x patch
-  nextcloudImage = "docker.io/library/nextcloud:34.0.0";
+  nextcloudImage = "docker.io/library/nextcloud:34.0.1";
   clamavImage = "docker.io/clamav/clamav:stable";
   whiteboardImage = "ghcr.io/nextcloud-releases/whiteboard:stable";
   whiteboardPort = 3002;
