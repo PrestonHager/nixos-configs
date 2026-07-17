3e4ee98023077fd2a0f77a122e1b233773c40e27
Preston Hager <preston@hagerfamily.com>
2026-07-17 04:02:14 -0600

    ace: bump zitadel to 4.16.1
    Automated patch/minor update triggered by ace-service-auto-update.

--- nixos/containers/zitadel.nix
+++ nixos/containers/zitadel.nix
@@ -189,7 +189,7 @@ in {
   virtualisation.oci-containers.containers.zitadel = {
     autoStart = true;
     dependsOn = [ "zitadel-db" ];
-    image = "ghcr.io/zitadel/zitadel:v4.16.0";
+    image = "ghcr.io/zitadel/zitadel:v4.16.1";
     cmd = [
       "start-from-init"
       "--masterkeyFromEnv"
@@ -220,7 +220,7 @@ in {
   virtualisation.oci-containers.containers.zitadel-login = {
     autoStart = true;
     dependsOn = [ "zitadel" ];
-    image = "ghcr.io/zitadel/zitadel-login:v4.16.0";
+    image = "ghcr.io/zitadel/zitadel-login:v4.16.1";
     extraOptions = [ "--pod=zitadel-pod" ];
     environment = {
       ZITADEL_API_URL = "http://127.0.0.1:8080";
