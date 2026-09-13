c628d6db19d07e79091899648661a25b32294540
Preston Hager <preston@hagerfamily.com>
2026-09-13 14:49:53 -0600

    ace: bump vaultwarden to 1.37.3
    Automated patch/minor update triggered by ace-service-auto-update.

--- nixos/containers/vaultwarden.nix
+++ nixos/containers/vaultwarden.nix
@@ -57,7 +57,7 @@ in
     ];
 
     # Finally, the vaultwarden image and version
-    image = "ghcr.io/dani-garcia/vaultwarden:1.37.2";
+    image = "ghcr.io/dani-garcia/vaultwarden:1.37.3";
   };
 }
 
